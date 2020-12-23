021a0d53b3a614e746a2a5b75054ba9f826f5358
leska91 <mayu1101u@gmail.com>
2020-12-23 22:59:15 +0900

    Userモデルに各カラムのバリデーションを記述した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: ture
+  validates :profile, presence: ture
+  validates :occupation, presence: ture
+  validates :position, presence: ture
 end
