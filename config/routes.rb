d7a02cb1d73520e3111385bdc8e220c9d3e9ccea
leska91 <mayu1101u@gmail.com>
2020-12-28 23:54:23 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
   get '/users/sign_up', to: 'registrations#new'
+  get '/users/sign_in', to: 'sessions#new'
+  get '/users/sign_out', to: 'sessions#destroy'
 end
